e3d0fc93b57b4aebfa5d27ff2a9e69ee102e3991
Paveltrety <pasha57@bk.ru>
2021-01-06 15:39:56 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowseRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,11 +8,11 @@ import { Provider } from 'react-redux';
 import store from './redux/store'
 
 ReactDOM.render(
- <HashRouter>
+ <BrowseRouter>
   <Provider store ={store}>
     <App />
     </Provider>
-    </HashRouter>,
+    </BrowseRouter>,
   document.getElementById('root')
 );
 
